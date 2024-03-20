63cad88db43aedcf4d9ec7e72e35c177ded2f20f
SprilerBr <Djedrzejczyk88@gmail.com>
2024-03-20 16:43:23 -0300

    Ajustes na animação dos botões e adição dos projetos corretos

--- src/app/modules/portifolio/components/experiences/experiences.component.ts
+++ src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -6,14 +6,16 @@ import { IExperiences } from '../../interfaces/IExperiences.interface';
   standalone: true,
   imports: [],
   templateUrl: './experiences.component.html',
-  styleUrl: './experiences.component.scss'
+  styleUrl: './experiences.component.scss',
 })
 export class ExperiencesComponent {
-  public arrayExperiences = signal<IExperiences[]>([{
-    summary: {
-      strong: "Programador Aprendiz Back-End",
-      p: "Benner Sistemas | 2023 - 2024"
+  public arrayExperiences = signal<IExperiences[]>([
+    {
+      summary: {
+        strong: 'Programador Aprendiz Back-End',
+        p: 'Benner Sistemas | 2023 - 2024',
+      },
+      text: '<p>Nesse período atuei como desenvolvedor BackEnd usando C#, Delphi e VB.NET. Trabalhei em produto ERP e Logística me destacando na parte de conversão de desktop para web. Neste tempo na Benner aprendi como aplicar os conceitos de SOLID e TDD durante uma conversão na logística, onde começamos a conversão do zero de um modulo na Web que só tinha no desktop. Sobre versionamento de Git sempre fui bem orientado de como deveria ser os padrões de mensagens e como enviar as alterações, seja via linha de comando ou pela própria IDE.</p>',
     },
-    text: "<p>Nesse período atuei como desenvolvedor BackEnd usando C#, Delphi e VB.NET. Trabalhei em produto ERP e Logística me destacando na parte de conversão de desktop para web.</p>"
-  }])
+  ]);
 }
--- src/app/modules/portifolio/components/projects/projects.component.ts
+++ src/app/modules/portifolio/components/projects/projects.component.ts
@@ -23,17 +23,32 @@ export class ProjectsComponent {
 
   public arrayProjects = signal<IProjects[]>([
     {
-      src: 'assets/img/projects/vfull.png',
+      src: 'assets/img/projects/csharp3.png',
       alt: 'Projeto Controle de Estacionamento',
       title: 'Controle de Estacionamento',
       with: '100px',
-      height: '51px',
+      height: '100px',
       description:
         '<p>Um projeto simples de CRUD usando .NET Core 6.0, onde eu crio uma API e o frontend é feito em MVC ASP.NET Core 6.0. As tecnologias usadas são: Entity Framework para o banco de dados, FluentValidation para validação do código. Foi adotado o melhor padrão de repositório que eu conhecia no momento, dividindo em camadas como, camada das entidades, camada do repositorio camada da validação, e adotei o TDD, para fazer meu codigo direcionado a testes.</p>',
       links: [
         {
           name: 'Conheça o projeto!',
           href: 'https://github.com/DaviJedrzejczyk/Controle-De-Estacionamento',
+        },
+      ],
+    },
+    {
+      src: 'assets/img/projects/csharp.png',
+      alt: 'Projeto Integrador do meu Ensino Médio, PI Esporte Kindle',
+      title: 'PI - Esporte Kindle',
+      with: '100px',
+      height: '100px',
+      description:
+        '<p>Um projeto integrador do meu ensino médio com técnico integrado que eu fiz com outros 2 participantes, um sistema de Controle de Estoque com CRUD completo e totalmente funcional. Usamos as tecnologias .NET Core 6.0 com WindowsForms, EntityFramework para o banco de dados e FluentValidation para a validação. O padrão de repositório usado foi MVC.</p>',
+      links: [
+        {
+          name: 'Conheça o projeto!',
+          href: 'https://github.com/DaviJedrzejczyk/Pi---Esporte.Kindle',
         },
       ],
     },
